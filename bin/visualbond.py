66cda3d42e1279e0178d05ed31d393a042e6c0e4
mmatera <matera@fisica.unlp.edu.ar>
2018-09-24 07:14:14 -0300

    fixing evaluation of errors in chi panel

--- bin/visualbond.py
+++ bin/visualbond.py
@@ -1091,7 +1091,7 @@ class ApplicationGUI:
             chitext = (chitext + textmarkers["Delta_symbol"][fmt] +
                        "E" + textmarkers["sub_symbol"][fmt] + str(j + 1) +
                        textmarkers["equal_symbol"][fmt] + " " +
-                       str(chi) + " " + textmarkers["open_comment"][fmt] +
+                       str(chi*tolerance) + " " + textmarkers["open_comment"][fmt] +
                        labels[j] +
                        textmarkers["close_comment"][fmt] +
                        textmarkers["separator_symbol"][fmt] + "\n")
